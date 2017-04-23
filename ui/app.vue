15100f4bb145aa700df9f87c842c654118e5954d
Matthias Loibl <mail@matthiasloibl.com>
2017-04-24 01:58:47 +0200

    Add everything necessary to render a vue component

---
+++ ui/app.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container-fluid">
+        <h3>{{ name }}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data () {
+            return {
+                name: 'gitloud',
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .container-fluid h3 {
+        font-family: sans-serif;
+    }
+</style>
