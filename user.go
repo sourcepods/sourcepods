1f6809385119b0306bc0a57ec95362244f199698
Matthias Loibl <mail@matthiasloibl.com>
2017-04-28 01:39:25 +0200

    Add basic validation to user model

--- user.go
+++ user.go
@@ -1,9 +1,18 @@
 package gitloud
 
+import (
+	"gopkg.in/asaskevich/govalidator.v6"
+)
+
 type User struct {
-	ID       string `json:"id"`
-	Username string `json:"username"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
+	ID       string `json:"id" valid:"required,uuidv4"`
+	Username string `json:"username" valid:"required,alphanum,length(4|32)"`
+	Name     string `json:"name" valid:"required"`
+	Email    string `json:"email" valid:"required,email"`
 	Password string `json:"-"`
+}
+
+func (u *User) Validate() error {
+	_, err := govalidator.ValidateStruct(u)
+	return err
 }
