683413fa9fd97ed54fafd595f343990f211ce6f1
Matthias Loibl <mail@matthiasloibl.com>
2017-04-30 10:46:10 +0200

    Add a first try to implement http server graceful shutdown

--- cmd/gitpod/web.go
+++ cmd/gitpod/web.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gitpods/gitpod/handler"
 	"github.com/gitpods/gitpod/store"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	"github.com/gobuffalo/packr"
+	"github.com/oklog/oklog/pkg/group"
 	"github.com/urfave/cli"
 )
 
@@ -19,18 +22,34 @@ func ActionWeb(c *cli.Context) error {
 	// Create the logger based on the environment: production/development/test
 	logger := newLogger(env)
 
-	// Create FileServer handler with buffalo's packr to serve file from disk or from within the binary.
-	// The path is relative to this file.
-	box := packr.NewBox("../../public")
+	var server *http.Server
 
-	// Create a simple store running in memory for example purposes
-	userStore := store.NewUserInMemory()
+	var gr group.Group
+	gr.Add(func() error {
+		// Create FileServer handler with buffalo's packr to serve file from disk or from within the binary.
+		// The path is relative to this file.
+		box := packr.NewBox("../../public")
 
-	// Create the http router and return it for use
-	r := handler.NewRouter(logger, box, userStore)
+		// Create a simple store running in memory for example purposes
+		userStore := store.NewUserInMemory()
 
-	level.Info(logger).Log("msg", "starting gitpod", "addr", addr)
-	return http.ListenAndServe(addr, r)
+		// Create the http router and return it for use
+		r := handler.NewRouter(logger, box, userStore)
+
+		level.Info(logger).Log("msg", "starting gitpod", "addr", addr)
+
+		server := &http.Server{Addr: addr, Handler: r}
+		return server.ListenAndServe()
+	}, func(err error) {
+		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+		server.Shutdown(ctx)
+		level.Info(logger).Log(
+			"msg", "http server shutdown gracefully",
+			"err", err,
+		)
+	})
+
+	return gr.Run()
 }
 
 func newLogger(env string) log.Logger {
