e4eab2a30285fdf7c8a5048e55eabfc1ef08105e
Matthias Loibl <mail@matthiasloibl.com>
2017-05-01 15:41:06 +0200

    Improve UI of user/list.vue

--- ui/user/list.vue
+++ ui/user/list.vue
@@ -1,49 +1,66 @@
 <template>
-    <div class="uk-container">
-        <p>users:</p>
-        <ul>
-            <li v-for="user in users">
-                {{ user.name }} - {{ user.email }} -
+	<div class="uk-container">
 
+		<h3>Users</h3>
 
-                <router-link :to="`/users/${user.username}`">profile</router-link>
-                <router-link :to="`/users/${user.username}/edit`">edit</router-link>
-                <span @click="deleteUser(user)">delete</span>
-            </li>
-        </ul>
-    </div>
+		<ul class="uk-list uk-list-divider">
+			<li v-for="user in users">
+				<div class="uk-grid">
+					<div>
+						<gravatar class="uk-border-circle" :email="user.email" :size="46" default-img="mm"></gravatar>
+					</div>
+					<div class="uk-width-expand">
+						<router-link class="uk-link-reset uk-text-bold" :to="`/users/${user.username}`">
+							{{user.name}}
+						</router-link>
+						<br>
+						<span>{{ user.email }}</span>
+					</div>
+					<div>
+						<router-link class="uk-button uk-button-default" :to="`/users/${user.username}/edit`">
+							edit
+						</router-link>
+						<button class="uk-button uk-button-danger" @click="deleteUser(user)">delete</button>
+					</div>
+				</div>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-    import axios from 'axios';
+	import axios from 'axios';
+	import Gravatar from 'vue-gravatar';
 
-    export default {
-        name: 'user-list',
-        data() {
-            return {}
-        },
-        computed: {
-            users() {
-                return this.$store.state.users;
-            }
-        },
-        created(){
-            axios.get('/api/users')
-                .then((res) => {
-                    this.$store.commit('addUsers', res.data);
-                })
-                .catch((err) => {
-                    console.log(err);
-                })
-        },
-        methods: {
-            deleteUser(user) {
-                if (confirm(`Do you really want to delete ${user.username}?`)) {
-                    this.$store.dispatch('deleteUser', user.username);
-                }
-            }
-        }
-    }
+	export default {
+		components: {
+			Gravatar,
+		},
+		data() {
+			return {}
+		},
+		computed: {
+			users() {
+				return this.$store.state.users;
+			}
+		},
+		created(){
+			axios.get('/api/users')
+				.then((res) => {
+					this.$store.commit('addUsers', res.data);
+				})
+				.catch((err) => {
+					console.log(err);
+				})
+		},
+		methods: {
+			deleteUser(user) {
+				if (confirm(`Do you really want to delete ${user.username}?`)) {
+					this.$store.dispatch('deleteUser', user.username);
+				}
+			}
+		}
+	}
 </script>
 
 <style>
