11cd2b918779c509f6906efc7d053d585575ab9f
Matthias Loibl <mail@matthiasloibl.com>
2017-05-08 01:59:01 +0200

    When running stratus dev run webpack, api & ui all in a group

--- cmd/stratus/main.go
+++ cmd/stratus/main.go
@@ -6,10 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strings"
-	"time"
 
-	"github.com/fsnotify/fsnotify"
 	"github.com/oklog/oklog/pkg/group"
 	"github.com/urfave/cli"
 )
@@ -21,12 +18,13 @@ func main() {
 
 	app.Commands = []cli.Command{{
 		Name:   "dev",
-		Usage:  "Runs gitpods in development mode",
+		Usage:  "Runs gitpods on you local development machine",
 		Action: actionDev,
 		Flags: []cli.Flag{
-			cli.StringFlag{Name: "addr", Usage: "The address to run gitpods on", Value: ":3000"},
+			cli.StringFlag{Name: "addr-ui", Usage: "The address to run the UI on", Value: ":3000"},
+			cli.StringFlag{Name: "addr-api", Usage: "The address to run the API on", Value: ":3010"},
 			cli.StringFlag{Name: "env", Usage: "Set the env gitpods runs in", Value: "development"},
-			cli.StringFlag{Name: "loglevel", Usage: "The log level to filter logs with before printing", Value: "debug"},
+			cli.StringFlag{Name: "log-level", Usage: "The log level to filter logs with before printing", Value: "debug"},
 		},
 	}}
 
@@ -36,141 +34,65 @@ func main() {
 }
 
 func actionDev(c *cli.Context) error {
-	addrFlag := c.String("addr")
+	uiAddrFlag := c.String("addr-ui")
+	apiAddrFlag := c.String("addr-api")
 	envFlag := c.String("env")
-	loglevelFlag := c.String("loglevel")
-
-	env := []string{
-		fmt.Sprintf("GITPODS_ADDR=%s", addrFlag),
-		fmt.Sprintf("GITPODS_ENV=%s", envFlag),
-		fmt.Sprintf("GITPODS_LOGLEVEL=%s", loglevelFlag),
-	}
+	loglevelFlag := c.String("log-level")
 
 	var g group.Group
-	g.Add(RunGitpod(env), func(err error) {
-		if err != nil {
-			log.Fatal(err)
-			return
+	{
+		webpack := &WebpackRunner{}
+		g.Add(func() error {
+			log.Println("starting webpack")
+			return webpack.Run()
+		}, func(err error) {
+			log.Println("stopping webpack")
+			webpack.Stop()
+		})
+	}
+	{
+		ui := &UIRunner{}
+		env := []string{
+			fmt.Sprintf("GITPODS_ADDR=%s", uiAddrFlag),
+			fmt.Sprintf("GITPODS_ENV=%s", envFlag),
+			fmt.Sprintf("GITPODS_LOGLEVEL=%s", loglevelFlag),
 		}
-	})
 
-	g.Add(RunWebpack, func(err error) {
-		if err != nil {
-			log.Fatal(err)
-			return
+		g.Add(func() error {
+			log.Println("starting ui")
+			return ui.Run(env)
+		}, func(err error) {
+			log.Println("stopping ui")
+			ui.Stop()
+		})
+	}
+	{
+		ui := &APIRunner{}
+		env := []string{
+			fmt.Sprintf("GITPODS_ADDR=%s", apiAddrFlag),
+			fmt.Sprintf("GITPODS_ENV=%s", envFlag),
+			fmt.Sprintf("GITPODS_LOGLEVEL=%s", loglevelFlag),
 		}
-	})
+
+		g.Add(func() error {
+			log.Println("starting api")
+			return ui.Run(env)
+		}, func(err error) {
+			log.Println("stopping api")
+			ui.Stop()
+		})
+	}
+
+	//TODO: Add actor to the group that listens for system call to stop the group gracefully
 
 	return g.Run()
 }
 
-// RunGitpod runs a development server and restarts it with a new build if files change.
-func RunGitpod(env []string) func() error {
-	return func() error {
-		builds := make(chan bool)
-
-		go BuildForever(builds)
-
-		go func() {
-			if err := build(); err == nil {
-				builds <- true
-			}
-		}()
-
-		var cmd *exec.Cmd
-		for {
-			<-builds
-			if cmd != nil {
-				cmd.Process.Kill()
-			}
-
-			cmd = exec.Command("./dist/gitpods")
-			go func() {
-				cmd.Env = env
-				cmd.Stdin = os.Stdin
-				cmd.Stdout = os.Stdout
-				cmd.Stderr = os.Stderr
-				if err := cmd.Run(); err != nil {
-					log.Println(err)
-					return
-				}
-			}()
-		}
-
-		return nil
-	}
+type WebpackRunner struct {
+	cmd *exec.Cmd
 }
 
-// BuildForever watches the filesystem and builds a new binary if something changes.
-// It notifies a channel that a build was created
-func BuildForever(builds chan<- bool) {
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer watcher.Close()
-
-	go func() {
-		for {
-			select {
-			case event := <-watcher.Events:
-				if event.Op != fsnotify.Chmod && event.Name != "" {
-					start := time.Now()
-					if err := build(); err == nil { // only notify and log if binary was created successfully.
-						log.Println("built a new binary in", time.Since(start))
-						builds <- true // notify that a new build was successfully created
-					}
-					watcher.Remove(event.Name)
-					watcher.Add(event.Name)
-				}
-			case err := <-watcher.Errors:
-				log.Println(err)
-			}
-		}
-	}()
-
-	files, err := findGoFiles()
-	if err != nil {
-		log.Println(err)
-	}
-
-	for _, file := range files {
-		if err := watcher.Add(filepath.Join(".", file)); err != nil {
-			log.Println(err)
-		}
-	}
-
-	select {}
-}
-
-func findGoFiles() ([]string, error) {
-	var files []string
-	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-		if strings.HasPrefix(path, "cmd/stratus") { // don't watch stratus itself
-			return nil
-		}
-		if strings.HasSuffix(path, ".go") {
-			files = append(files, path)
-		}
-		return nil
-	})
-	return files, err
-}
-
-func build() error {
-	cmd := exec.Command("go", "build", "-v", "-i", "-o", "./dist/gitpods", "./cmd/gitpods")
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		return err
-	}
-	return nil
-}
-
-// RunWebpack starts webpack in the background and watches for changes
-func RunWebpack() error {
+func (r *WebpackRunner) Run() error {
 	file := "./webpack.config.js"
 	_, err := os.Stat(file)
 	if err != nil {
@@ -178,9 +100,63 @@ func RunWebpack() error {
 		return nil
 	}
 
-	cmd := exec.Command(filepath.Join("node_modules", ".bin", "webpack"), "--watch")
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
+	r.cmd = exec.Command(filepath.Join("node_modules", ".bin", "webpack"), "--watch")
+	r.cmd.Stdin = os.Stdin
+	r.cmd.Stdout = os.Stdout
+	r.cmd.Stderr = os.Stderr
+
+	return r.cmd.Run()
+}
+
+func (r *WebpackRunner) Stop() {
+	if r.cmd == nil || r.cmd.Process == nil {
+		return
+	}
+	r.cmd.Process.Kill()
+}
+
+type UIRunner struct {
+	cmd *exec.Cmd
+}
+
+func (r *UIRunner) Run(env []string) error {
+	//TODO: run build command if no binary available
+	r.cmd = exec.Command("./dist/ui")
+
+	fmt.Printf("api env: %+v\n", env)
+	r.cmd.Env = env
+	r.cmd.Stdin = os.Stdin
+	r.cmd.Stdout = os.Stdout
+	r.cmd.Stderr = os.Stderr
+	return r.cmd.Run()
+}
+
+func (r *UIRunner) Stop() {
+	if r.cmd == nil || r.cmd.Process == nil {
+		return
+	}
+	r.cmd.Process.Kill()
+}
+
+type APIRunner struct {
+	cmd *exec.Cmd
+}
+
+func (r *APIRunner) Run(env []string) error {
+	//TODO: run build command if no binary available
+	r.cmd = exec.Command("./dist/api")
+
+	fmt.Printf("api env: %+v\n", env)
+	r.cmd.Env = env
+	r.cmd.Stdin = os.Stdin
+	r.cmd.Stdout = os.Stdout
+	r.cmd.Stderr = os.Stderr
+	return r.cmd.Run()
+}
+
+func (r *APIRunner) Stop() {
+	if r.cmd == nil || r.cmd.Process == nil {
+		return
+	}
+	r.cmd.Process.Kill()
 }
