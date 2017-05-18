54aa7e98df0dd8bd94b8bcf55d1cea205729fa96
Matthias Loibl <mail@matthiasloibl.com>
2017-05-18 13:02:11 +0200

    Create a user.loggingService to log user service events with go-kit's logger

---
+++ user/logging.go
@@ -0,0 +1,116 @@
+package user
+
+import (
+	"time"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+)
+
+type loggingService struct {
+	logger log.Logger
+	Service
+}
+
+func NewLoggingService(logger log.Logger, s Service) Service {
+	return &loggingService{logger, s}
+}
+
+func (s *loggingService) FindAll() ([]*User, error) {
+	start := time.Now()
+
+	users, err := s.Service.FindAll()
+
+	logger := log.With(s.logger,
+		"method", "FindAll",
+		"duration", time.Since(start),
+	)
+
+	if err != nil {
+		level.Warn(logger).Log("msg", "failed to find all users", "err", err)
+	} else {
+		level.Debug(logger).Log()
+	}
+
+	return users, err
+}
+
+func (s *loggingService) FindByUsername(username string) (*User, error) {
+	start := time.Now()
+
+	user, err := s.Service.FindByUsername(username)
+
+	logger := log.With(s.logger,
+		"method", "FindByUsername",
+		"duration", time.Since(start),
+		"username", username,
+	)
+
+	if err != nil {
+		level.Warn(logger).Log("msg", "failed to find user by username", "err", err)
+	} else {
+		level.Debug(logger).Log()
+	}
+
+	return user, err
+}
+
+func (s *loggingService) Create(user *User) (*User, error) {
+	start := time.Now()
+
+	user, err := s.Service.Create(user)
+
+	logger := log.With(s.logger,
+		"method", "Create",
+		"duration", time.Since(start),
+		"username", user.Username,
+	)
+
+	if err != nil {
+		level.Warn(logger).Log("msg", "failed to create user", "err", err)
+	} else {
+		level.Debug(logger).Log()
+	}
+
+	return user, err
+}
+
+func (s *loggingService) Update(username string, user *User) (*User, error) {
+	start := time.Now()
+
+	user, err := s.Service.Update(username, user)
+
+	logger := log.With(s.logger,
+		"method", "Update",
+		"duration", time.Since(start),
+		"username", username,
+	)
+
+	if err != nil {
+		level.Warn(logger).Log("msg", "failed to update user", "err", err)
+	} else {
+		level.Debug(logger).Log()
+	}
+
+	return user, err
+}
+
+func (s *loggingService) Delete(username string) error {
+	start := time.Now()
+
+	err := s.Service.Delete(username)
+
+	logger := log.With(s.logger,
+		"method", "Delete",
+		"duration", time.Since(start),
+		"username", username,
+	)
+
+	if err != nil {
+		level.Warn(logger).Log("msg", "failed to update user", "err", err)
+	} else {
+		level.Debug(logger).Log()
+	}
+
+	return err
+}
