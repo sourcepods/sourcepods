30c7989834950893fec3aa6f9fb255f954ad757c
Matthias Loibl <mail@matthiasloibl.com>
2017-05-23 09:44:01 +0200

    Add badges to Issue & Pull Requests

--- ui/navbar.vue
+++ ui/navbar.vue
@@ -7,11 +7,17 @@
 			</router-link>
 
 			<ul class="uk-navbar-nav">
-				<li class="uk-active">
-					<router-link to="/pulls">Pull Requests</router-link>
+				<li>
+					<router-link to="/issues">
+						Issues
+						<span class="uk-badge">12</span>
+					</router-link>
 				</li>
 				<li>
-					<router-link to="/issues">Issues</router-link>
+					<router-link to="/pulls">
+						Pull Requests
+						<span class="uk-badge">1</span>
+					</router-link>
 				</li>
 			</ul>
 
@@ -68,4 +74,13 @@
 </script>
 
 <style scoped>
+	ul.uk-navbar-nav span.uk-badge {
+		height: 15px;
+		width: 15px;
+		min-width: 15px;
+		line-height: 15px;
+		font-size: 0.6rem;
+		margin-left: 5px;
+		margin-bottom: 10px;
+	}
 </style>
