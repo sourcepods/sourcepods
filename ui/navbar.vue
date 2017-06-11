e44b8a46240d94d8953f34e99d7a8f2ed140b20c
Matthias Loibl <mail@matthiasloibl.com>
2017-06-11 17:14:32 +0200

    Improve mobile responsiveness of navbar

--- ui/navbar.vue
+++ ui/navbar.vue
@@ -1,32 +1,21 @@
 <template>
 	<div class="uk-navbar-container uk-margin">
 		<nav class="uk-container" uk-navbar>
-			<div class="uk-navbar-left">
 
+			<div class="uk-position-top-center uk-overlay" v-if="loading">
+				<div uk-spinner></div>
+			</div>
+
+			<div class="uk-navbar-left">
 				<router-link class="uk-navbar-item uk-logo" to="/">
 					<img src="/img/logo.svg" alt="" style="height: 46px;"/>
 				</router-link>
 
-				<ul class="uk-navbar-nav">
-					<li>
-						<router-link to="/issues">
-							Issues
-							<!--<span class="uk-badge">12</span>-->
-						</router-link>
-					</li>
-					<li>
-						<router-link to="/pulls">
-							Pull Requests
-							<!--<span class="uk-badge">1</span>-->
-						</router-link>
-					</li>
-				</ul>
-
-				<div uk-spinner v-if="loading"></div>
+				<main-nav class="uk-visible@s"></main-nav>
 
 			</div>
 
-			<div class="uk-navbar-right">
+			<div class="uk-navbar-right uk-flex-last@s">
 				<!--<div class="uk-navbar-item">-->
 				<!--<form class="uk-search uk-search-navbar">-->
 				<!--<span class="uk-search-icon-flip" uk-search-icon></span>-->
@@ -35,11 +24,13 @@
 				<!--</div>-->
 				<ul class="uk-navbar-nav" v-if="user !== null">
 					<li>
-						<router-link :to="`/${user.username}`">
-							<gravatar class="uk-border-circle" :email="user.email" :size="46"
-									  default-img="mm"></gravatar>
+						<div>
+							<router-link :to="`/${user.username}`">
+								<gravatar class="uk-border-circle" :email="user.email" :size="46"
+										  default-img="mm"></gravatar>
+							</router-link>
 							<span uk-icon="icon: more-vertical"></span>
-						</router-link>
+						</div>
 						<div class="uk-navbar-dropdown">
 							<ul class="uk-nav uk-navbar-dropdown-nav">
 								<li>
@@ -58,15 +49,24 @@
 				</ul>
 			</div>
 		</nav>
+
+		<nav class="uk-container uk-hidden@s" uk-navbar>
+			<div class="uk-navbar-left">
+				<main-nav class="uk-hidden@s"></main-nav>
+			</div>
+		</nav>
+
 	</div>
 </template>
 
 <script>
 	import Gravatar from 'vue-gravatar';
+	import MainNav from './navbar_mainnav.vue';
 
 	export default {
 		components: {
 			Gravatar,
+			MainNav,
 		},
 		created() {
 			this.$store.dispatch('fetchAuthenticatedUser')
@@ -87,15 +87,3 @@
 		},
 	}
 </script>
-
-<style scoped>
-	ul.uk-navbar-nav span.uk-badge {
-		height: 15px;
-		width: 15px;
-		min-width: 15px;
-		line-height: 15px;
-		font-size: 0.6rem;
-		margin-left: 5px;
-		margin-bottom: 10px;
-	}
-</style>
