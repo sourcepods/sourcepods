b95a6a55fb6f2bb7038f2d05648f0e740ef16c99
Matthias Loibl <mail@matthiasloibl.com>
2017-08-21 19:42:58 +0200

    Delete ui/ containing JavaScript

--- ui/issues/issues.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<div class="uk-container">
-		<h3>No Issues.</h3>
-	</div>
-</template>
--- ui/pulls/pulls.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<div class="uk-container">
-		<h3>No Pull Requests.</h3>
-	</div>
-</template>
