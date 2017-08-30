c5fd0581b97388d3aa3ce34af6896a7cfe57ebed
Matthias Loibl <mail@matthiasloibl.com>
2017-08-30 16:00:12 +0200

    Create a BrowserClient to inject into services

--- ui/web/main.dart
+++ ui/web/main.dart
@@ -1,6 +1,10 @@
+import 'package:angular2/angular2.dart';
 import 'package:angular2/platform/browser.dart';
+import 'package:http/browser_client.dart';
 import 'package:gitpods/app_component.dart';
 
 void main() {
-  bootstrap(AppComponent);
+  bootstrap(AppComponent, [
+    provide(BrowserClient, useFactory: () => new BrowserClient(), deps: [])
+  ]);
 }
