cea3079c930d2a95b5754e1a4ffd086c6cd49d1d
Matthias Loibl <mail@matthiasloibl.com>
2017-09-09 21:45:43 +0200

    Don't run webpack when running gitpods build

--- cmd/gitpods/build.go
+++ cmd/gitpods/build.go
@@ -7,17 +7,12 @@ import (
 func buildAction(c *cli.Context) error {
 	apiRunner := NewGitPodsRunner("api", []string{})
 	uiRunner := NewGitPodsRunner("ui", []string{})
-	webpack := &WebpackRunner{}
 
 	if err := apiRunner.Build(); err != nil {
 		return err
 	}
 
 	if err := uiRunner.Build(); err != nil {
-		return err
-	}
-
-	if err := webpack.Run(false); err != nil {
 		return err
 	}
 
