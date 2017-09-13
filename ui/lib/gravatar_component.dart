fc068add1739f16dfeab7304c4f030343cdbe90f
Matthias Loibl <mail@matthiasloibl.com>
2017-09-13 23:40:13 +0200

    Finally got gravatar working

--- ui/lib/gravatar_component.dart
+++ ui/lib/gravatar_component.dart
@@ -1,46 +1,24 @@
 import 'package:angular/angular.dart';
 import 'package:crypto/crypto.dart';
 
-
 @Component(
     selector: 'gravatar',
-    template: '''Bank Name: {{email}} - Account Id: {{size}}''')
-class Gravatar {
+    template: '<img class="{{classs}}" src="https://www.gravatar.com/avatar/{{hash}}?s={{size}}&d=mm&r=g"/>'
+)
+class Gravatar implements OnInit {
   @Input()
   String email;
 
   @Input()
-  String size;
+  String size = '64';
 
-//  Gravatar(String this.email, String this.size);
+  @Input('class')
+  String classs = '';
+
+  Digest hash;
+
+  @override
+  ngOnInit() {
+    this.hash = md5.convert(this.email.codeUnits);
+  }
 }
-
-//@Component(
-//  selector: 'gravatar',
-//  template: '<img src="https://www.gravatar.com/avatar/{{hash}}?s={{size}}&d=mm&r=g" alt="">',
-//)
-//class Gravatar implements OnInit {
-//  Digest hash;
-//
-//  @Attribute('email')
-//  String email;
-//  @Attribute('size')
-//  int size = 128;
-//
-//  Gravatar(this.email, this.size);
-//
-//  @override
-//  ngOnInit() {
-//    print('OnInit: ${this.email}');
-//    print('${this.email} - ${size}');
-//    hash = md5.convert('mail@matthiasloibl.com'.codeUnits);
-//  }
-//}
-
-//@Directive(selector: 'input')
-//class InputAttrDirective {
-//  InputAttrDirective(@Attribute('type') String type) {
-//    // type would be 'text' in this example
-//  }
-//}
-
--- ui/lib/user_list_component.dart
+++ ui/lib/user_list_component.dart
@@ -1,12 +1,13 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
+import 'package:gitpods/gravatar_component.dart';
 import 'package:gitpods/user.dart';
 import 'package:gitpods/user_service.dart';
 
 @Component(
   selector: 'gitpods-user-list',
   templateUrl: 'user_list_component.html',
-  directives: const [COMMON_DIRECTIVES, ROUTER_DIRECTIVES],
+  directives: const [COMMON_DIRECTIVES, ROUTER_DIRECTIVES, Gravatar],
   providers: const [UserService],
 )
 class UserListComponent implements OnInit {
