e45401cea68d34a5f0e3df343724384ed36653cb
Matthias Loibl <mail@matthiasloibl.com>
2017-09-28 21:59:01 +0200

    Add storage to gitpods build command

--- cmd/gitpods/build.go
+++ cmd/gitpods/build.go
@@ -6,9 +6,14 @@ import (
 
 func buildAction(c *cli.Context) error {
 	apiRunner := NewGitPodsRunner("api", []string{})
+	storageRunner := NewGitPodsRunner("storage", []string{})
 	uiRunner := NewGitPodsRunner("ui", []string{})
 
 	if err := apiRunner.Build(); err != nil {
+		return err
+	}
+
+	if err := storageRunner.Build(); err != nil {
 		return err
 	}
 
