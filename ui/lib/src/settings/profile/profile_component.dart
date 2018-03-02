57ed294eb91d4d7a76c38e7f93e96648973ed8ff
Matthias Loibl <mail@matthiasloibl.com>
2018-03-03 00:42:39 +0100

    Add LoadingComponent as directive to profile setting component

--- ui/lib/src/settings/profile/profile_component.dart
+++ ui/lib/src/settings/profile/profile_component.dart
@@ -3,13 +3,18 @@ import 'dart:html';
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 import 'package:angular_router/angular_router.dart';
+import 'package:gitpods/src/loading_component.dart';
 import 'package:gitpods/src/user/user.dart';
 import 'package:gitpods/src/user/user_service.dart';
 
 @Component(
   selector: 'profile',
   templateUrl: 'profile_component.html',
-  directives: const [COMMON_DIRECTIVES, formDirectives],
+  directives: const [
+    COMMON_DIRECTIVES,
+    formDirectives,
+    LoadingComponent,
+  ],
 )
 class ProfileComponent implements OnInit {
   final Router _router;
