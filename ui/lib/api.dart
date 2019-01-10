35b65cb1879fb4337d6aeea7f956842862090ebf
Matthias Loibl <mail@matthiasloibl.com>
2019-01-10 01:27:46 +0100

    ui: Add repositories to API

--- ui/lib/api.dart
+++ ui/lib/api.dart
@@ -7,8 +7,10 @@ class API {
     String url = getProperty(window, 'api');
     ApiClient client = new ApiClient(basePath: '$url/v1');
 
+    this.repositories = RepositoriesApi(client);
     this.users = UsersApi(client);
   }
 
+  RepositoriesApi repositories;
   UsersApi users;
 }
