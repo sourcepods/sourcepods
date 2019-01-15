b0f83c008a662d9932c75bebb4600eee000c442c
Matthias Loibl <mail@matthiasloibl.com>
2019-01-16 00:10:33 +0100

    Create dev/debug command: storage tree [owner name rev path]
    This is calling directly the same func as gRPC just via a CLI for quick
    access.

---
+++ cmd/storage/cli.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"text/tabwriter"
+
+	"github.com/gitpods/gitpods/pkg/storage"
+
+	"github.com/gitpods/gitpods/cmd"
+	"github.com/urfave/cli"
+)
+
+func treeAction(c *cli.Context) error {
+	args := c.Args()
+	if len(args) != 4 {
+		return errors.New("need exactly 4 arguments: owner name rev path")
+	}
+	owner, name, rev, path := args[0], args[1], args[2], args[3]
+
+	s, err := storage.NewStorage(c.GlobalString(cmd.FlagRoot))
+	if err != nil {
+		return err
+	}
+
+	entries, err := s.Tree(context.Background(), owner, name, rev, path)
+	if err != nil {
+		return err
+	}
+
+	w := tabwriter.NewWriter(os.Stdout, 3, 0, 3, ' ', tabwriter.TabIndent)
+	fmt.Fprintln(w, "Mode\tType\tObject\tPath")
+	for _, e := range entries {
+		fmt.Fprintf(w, "%s\t%s\t%s\t%s\n", e.Mode, e.Type, e.Object, e.Path)
+	}
+
+	return w.Flush()
+}
--- cmd/storage/main.go
+++ cmd/storage/main.go
@@ -15,6 +15,16 @@ func main() {
 	app.Action = storageAction
 	app.Flags = storageFlags
 
+	app.Commands = cli.Commands{
+		{
+			Name:        "tree",
+			Usage:       "Print the tree of a repository",
+			Description: "Print the tree of a repository at a given rev with a path\n\tExample: tree gitpods gitpods master pkg/api",
+			ArgsUsage:   "owner name rev path",
+			Action:      treeAction,
+		},
+	}
+
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
