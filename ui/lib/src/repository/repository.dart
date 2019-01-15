89662829734d6f1a7c0659ee93371ec17e3fb419
Matthias Loibl <mail@matthiasloibl.com>
2019-01-15 01:27:00 +0100

    ui: Remove unnecessary classes from repository package

--- ui/lib/src/repository/repository.dart
+++ ui/lib/src/repository/repository.dart
@@ -2,17 +2,6 @@ import 'package:gitpods/src/user/user.dart';
 import 'package:gitpods/src/api/api.dart' as api;
 
 class Repository {
-  String id;
-  String name;
-  String description;
-  String website;
-  String defaultBranch;
-  DateTime created;
-  DateTime updated;
-
-  User owner;
-  List<RepositoryBranch> branches;
-
   Repository({
     this.id,
     this.name,
@@ -35,59 +24,13 @@ class Repository {
     );
   }
 
-
-  factory Repository.fromJSON(Map<String, dynamic> data) {
-    Repository repository = new Repository(
-      id: data['id'],
-      name: data['name'],
-    );
-
-    data['description'] != null ? repository.description = data['description'] : '';
-    data['website'] != null ? repository.website = data['website'] : '';
-    data['defaultBranch'] != null ? repository.defaultBranch = data['defaultBranch'] : '';
-
-    if (data['owner'] != null) {
-      repository.owner = new User.fromJSON(data['owner']);
-    }
-
-    if (data['createdAt'] != null) {
-      repository.created = DateTime.parse(data['createdAt']);
-    }
-
-    if (data['updatedAt'] != null) {
-      repository.updated = DateTime.parse(data['updatedAt']);
-    }
-
-    if (data['branches'] != null) {
-      repository.branches = data['branches']
-          .map((json) => new RepositoryBranch.fromJSON(json))
-          .toList();
-    }
-
-    return repository;
-  }
-
-}
-
-class RepositoryBranch {
+  String id;
   String name;
-  String sha1;
-  String type;
-  bool protected;
+  String description;
+  String website;
+  String defaultBranch;
+  DateTime created;
+  DateTime updated;
 
-  RepositoryBranch({
-    this.name,
-    this.sha1,
-    this.type,
-    this.protected,
-  });
-
-  factory RepositoryBranch.fromJSON(Map<String, dynamic> data) {
-    return new RepositoryBranch(
-      name: data['name'],
-      sha1: data['sha1'],
-      type: data['type'],
-      protected: data['protected'],
-    );
-  }
+  User owner;
 }
--- ui/lib/src/user/user.dart
+++ ui/lib/src/user/user.dart
@@ -30,30 +30,4 @@ class User {
       updated: user.updatedAt,
     );
   }
-
-  factory User.fromJSON(Map<String, dynamic> data) {
-    User user = new User(
-      id: data['id'],
-      email: data['email'],
-      username: data['username'],
-    );
-
-    data['name'] != null ? user.name = data['name'] : '';
-
-    if (data['repositories'] != null) {
-      user.repositories = data['repositories']
-          .map((data) => new Repository.fromJSON(data))
-          .toList();
-    }
-
-    if (data['createdAt'] != null) {
-      user.created = DateTime.parse(data['createdAt']);
-    }
-
-    if (data['updatedAt'] != null) {
-      user.updated = DateTime.parse(data['updatedAt']);
-    }
-
-    return user;
-  }
 }
