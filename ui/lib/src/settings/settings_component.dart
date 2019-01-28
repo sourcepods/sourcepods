e446c2d054d3fb0dba86c6ad2980f0f327e91601
Matthias Loibl <mail@matthiasloibl.com>
2019-01-28 22:20:18 +0100

    ui: Fix active tab indicator for settings

--- ui/lib/src/settings/settings_component.dart
+++ ui/lib/src/settings/settings_component.dart
@@ -8,10 +8,31 @@ import 'package:sourcepods/src/settings/routes.dart';
   directives: const [routerDirectives],
   exports: [Routes],
 )
-class SettingsComponent {
+class SettingsComponent implements OnActivate {
+  String tab;
+
+  @override
+  void onActivate(RouterState previous, RouterState current) {
+    _setTab(current.path);
+  }
+
+  void _setTab(String path) {
+    String tab = 'profile'; // default tab
+    List<String> elements = path.split('/');
+
+    // if tab other than default selected use this one
+    if (elements.length > 2) {
+      tab = elements.elementAt(2);
+    }
+
+    this.tab = tab;
+  }
+
   String profileUrl() => RoutesPaths.profile.toUrl();
 
   String accountUrl() => RoutesPaths.account.toUrl();
 
   String securityUrl() => RoutesPaths.security.toUrl();
+
+  bool tabActive(String tab) => this.tab == tab;
 }
