984cfeeb0a8447ff3e1deaf7bc7ca012c6f44db6
Matthias Loibl <mail@matthiasloibl.com>
2019-01-29 22:46:59 +0100

    ui: Finish a working breadcrumb for repositories

--- ui/lib/src/repository/files/breadcrumb_component.dart
+++ ui/lib/src/repository/files/breadcrumb_component.dart
@@ -4,12 +4,18 @@ import 'package:angular_router/angular_router.dart';
 @Component(
   selector: 'files-breadcrumb',
   templateUrl: 'breadcrumb_component.html',
+  styleUrls: ['breadcrumb_component.css'],
   directives: [
     coreDirectives,
     routerDirectives,
   ],
 )
-class BreadcrumbComponent implements OnChanges{
+class BreadcrumbComponent implements OnChanges {
+  @Input()
+  String ownerName;
+  @Input()
+  String repositoryName;
+
   @Input()
   String path;
 
@@ -20,7 +26,13 @@ class BreadcrumbComponent implements OnChanges{
     this.elements = this.path.split('/');
   }
 
-  String changeDirectory(element) {
-    return '';
+  String changeRoot() {
+    return './$ownerName/$repositoryName';
+  }
+
+  String changeDirectory(int i) {
+    String currentBranch = 'master'; // TODO
+    String newPath = path.split('/').getRange(0, i+1).join('/');
+    return './$ownerName/$repositoryName/tree/$currentBranch/$newPath';
   }
 }
